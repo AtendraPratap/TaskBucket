c231ec9518a627915ccabe62599b711787257faa
Atendra <atendrardr@gmail.com>
2020-12-19 16:29:37 +0530

    MyTasks

---
+++ tasks/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+# Create your views here.
+
+from .models import *
+from .forms import *
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks':tasks, 'form':form}
+    return render(request,'tasks/list.html',context)
+
+def updatetask(request,pk):
+    tasks = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=tasks)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=tasks)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form':form}
+
+    return render(request,'tasks/update_task.html',context)
